667fdb00acec0e7efb81cb8d309d238b903f1acb
Vitor de Sousa <10253164+vitordesousa@users.noreply.github.com>
2021-04-20 15:12:16 -0300

    inserção de fonts do google, melhora no código e finalização da primeira tela

--- App.tsx
+++ App.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { Welcome } from './src/pages/Welcome';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost';
 
+/* 
+	Anotações:
+	AppLoading Serve para segurar na tela do loading (de acordo com o condição ali embaixo) enquanto as fontes pré-destinadas não estão carregadas
+*/
 export default function App() {
+	let [ fontsLoaded ] = useFonts({
+		Jost_400Regular, 
+		Jost_600SemiBold
+	})
+
+	if(!fontsLoaded){
+		return <AppLoading />
+	}
+
 	return (
 		<Welcome />
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
 /* #missaoespacial */
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,37 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, Image, SafeAreaView, TouchableOpacity, Dimensions } from 'react-native';
-import colors from '../styles/colors';
+import { StyleSheet, Text, Image, SafeAreaView, TouchableOpacity, Dimensions, View } from 'react-native';
+import colors from '../styles/colors'; // Tem um arquivo chamado colors em /src/styles/colors.ts que é responsável por ter todas as cores do projeto. Ao ser importado é só usar colors.nomeDaCor que ele já funciona
+import fonts from '../styles/fonts'; // Tem um arquivo chamado fonts em /src/styles/fonts.ts que é responsável por ter todas as fontes do projeto. Ao ser importado é só usar fonts.nomeDaCor que ele já funciona
 import {Feather} from '@expo/vector-icons'
 
 import wateringImg from '../assets/watering.png'
 
 export function Welcome(){
-
 	/*
 		Anotações: 
-		{'\n'} é usado para  pular linha dentro do texto, já que a tag <br> não funciona
-		Tem um arquivo chamado colors em /src/styles/colors.ts que é responsável por ter todas as cores do projeto. Ao ser importado é só usar colors.nomeDaCor que ele já funciona
+		{'\n'} é usado para  pular linha dentro do texto, já que a tag <br> não funciona		
 		Criei um arquivo na raiz com o nome custom.d.ts que é um arquivo de configurações onde ele para dar dar erro em todas as imagens ".png"
+		SafeAreaView serve para que todos os elementos estejam dentro da área de visualização, e não talvez dentro da tabbar, ou de algum botão físico, ou do fone do celular
 	*/
-
 	return (
-		<SafeAreaView style={styles.container}>
-			<Text style={styles.title}>
-				Gerencie{'\n'}
-				suas plantas{'\n'}
-				de forma fácil
-			</Text>
+		<SafeAreaView style={styles.container}> 
+			<View  style={styles.wrapper}>
+				<Text style={styles.title}>
+					Gerencie{'\n'}
+					suas plantas de{'\n'}
+					forma fácil
+				</Text>
 
-			<Image source={wateringImg}  style={styles.image} resizeMode="contain"/>
+				<Image source={wateringImg}  style={styles.image} resizeMode="contain"/>
 
-			<Text style={styles.subtitle}>
-				Não esqueça mais de regar suas plantas.{'\n'}
-				Nós cuidamos de lembrar você sempre que precisar.
-			</Text>
+				<Text style={styles.subtitle}>
+					Não esqueça mais de regar suas plantas.{'\n'}
+					Nós cuidamos de lembrar você sempre que precisar.
+				</Text>
 
-			<TouchableOpacity style={styles.button} activeOpacity={0.7}>
-				<Feather name="chevron-right"  style={styles.buttonIcon} />
-			</TouchableOpacity>
+				<TouchableOpacity style={styles.button} activeOpacity={0.7}>
+					<Feather name="chevron-right"  style={styles.buttonIcon} />
+				</TouchableOpacity>
+			</View>
 		</SafeAreaView>
 	)
 }
@@ -39,20 +40,26 @@ export function Welcome(){
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+	},
+	wrapper: {
+		flex: 1,
 		alignItems: 'center',
 		justifyContent: 'space-around',
+		paddingHorizontal: 20
 	},
 	title: {
-		fontSize : 32,
-		fontWeight : 'bold',
-		textAlign : 'center',
 		color: colors.heading,
-		marginTop : 38
+		fontFamily: fonts.heading,
+		fontSize : 28,
+		lineHeight: 34,
+		marginTop : 38,
+		textAlign : 'center',
 	},
 	image: {
-		height : Dimensions.get('window').width * 0.7,
+		height : Dimensions.get('window').width * 0.7, // Dimensions serve para conseguir pegar o tamanho da tela do dispositivo, assim você não precisa colocar um tamanho fixo para a imagem, já que ela vai se adaptar automaticamente de acordo com o mesmo
 	},
 	subtitle: {
+		fontFamily: fonts.text,
 		textAlign : 'center',
 		fontSize : 18,
 		paddingHorizontal : 20,
