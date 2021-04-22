d2171b02cdf30ec1bbd3d376a2f0e87c8da006b2
Vitor de Sousa <10253164+vitordesousa@users.noreply.github.com>
2021-04-21 22:17:09 -0300

    colocando as plantas para aparecerem

--- src/components/PlantCardPrimary.tsx
+++ src/components/PlantCardPrimary.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, Image, View  } from 'react-native';
+import { StyleSheet, Text, View  } from 'react-native';
 import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
+import { SvgFromUri } from 'react-native-svg';
 
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
-
 
 interface PlantProps extends RectButtonProps {
 	data: {
@@ -16,6 +16,7 @@ interface PlantProps extends RectButtonProps {
 export const PlantCardPrimary = ({ data, ... rest} : PlantProps) => {
 	return (
 		<RectButton style={styles.container} { ... rest}>
+			<SvgFromUri uri={ data.photo } width={70} height={70} />
 			<Text style={styles.text}> { data.name } </Text>
 		</RectButton>
 	)
