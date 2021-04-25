9e034bcdde9a410118e47c157be6c1fd78bc78fd
Vitor de Sousa <10253164+vitordesousa@users.noreply.github.com>
2021-04-25 18:13:56 -0300

    faz a validação se o usuário digitou o nome

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -1,6 +1,6 @@
 import { useNavigation } from '@react-navigation/core';
 import React, { useState } from 'react';
-import { StyleSheet, SafeAreaView, View, Text, TextInput, KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { StyleSheet, SafeAreaView, View, Text, TextInput, KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
 import { Button } from '../components/Button';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
@@ -33,6 +33,11 @@ export function UserIdentification() {
 	const navigation = useNavigation();
 
 	function handleSubmit(){
+
+		if(!name){ // valida se o usuário digitou o nome
+			return Alert.alert('Me diz como chamar você ... 😥');
+		}
+
 		navigation.navigate('Confirmation')
 	}
 
