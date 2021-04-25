67bef77b733ff3bc4ae2f7306ee88801a5839687
Vitor de Sousa <10253164+vitordesousa@users.noreply.github.com>
2021-04-25 20:22:07 -0300

    fazendo estilização inicial da pagina da planta

--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Alert, StyleSheet, Text, View, Image, ScrollView, Platform, TouchableOpacity } from 'react-native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 import { SvgFromUri } from 'react-native-svg'
 
 import waterdrop from '../assets/waterdrop.png';
 import { Button } from '../components/Button';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function PlantSave(){
 	return (
@@ -12,13 +14,13 @@ export function PlantSave(){
 			<View style={styles.plantInfo}>
 				<SvgFromUri uri="" height={150} width={150} />
 				<Text style={styles.plantName}>Nome Da Planta</Text>
-				<Text style={styles.plantDescription}> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi rerum ut, tempore, velit sit iste similique illum ad a distinctio corporis voluptatum asperiores obcaecati molestias ab magnam sapiente, quos nemo?</Text>
+				<Text style={styles.plantDescription}> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi rerum ut, tempore, velit sit iste similique illum ad a distinctio </Text>
 			</View>
 			<View style={styles.controller}>
 				<View style={styles.tipContainer}>
 					<Image source={waterdrop} style={styles.tipImage} />
 					<Text style={styles.tipText}>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quod eligendi aliquam consectetur unde et, est atque, voluptate blanditiis, ullam soluta ipsum numquam sequi porro totam facere! Odio, dolore dolorum!
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
 					</Text>
 				</View>
 				<Text style={styles.alertLabel}>
@@ -39,28 +41,60 @@ const styles = StyleSheet.create({
 		backgroundColor: colors.shape
 	},
 	plantInfo :{
-
+		flex: 1,
+		paddingHorizontal: 30,
+		paddingVertical: 50,
+		alignItems : 'center',
+		justifyContent: 'center',
+		backgroundColor: colors.shape,
 	},
 	plantName : {
-
+		fontFamily : fonts.heading,
+		fontSize: 24,
+		color: colors.heading,
+		marginTop: 15
 	},
 	plantDescription :{
-
+		textAlign : 'center',
+		fontFamily: fonts.text,
+		fontSize : 17,
+		color: colors.heading,
+		marginTop: 10
 	},
 	controller :{
-
+		backgroundColor : colors.white,
+		paddingHorizontal: 20,
+		paddingTop: 20,
+		paddingBottom : getBottomSpace() || 20 // se estiver no iphone irá retornar o valor necessário, se estiver no android, irá colocar 20
 	},
 	tipContainer :{
-
+		flexDirection : 'row',
+		justifyContent : 'space-between',
+		alignItems : 'center',
+		padding: 20,
+		backgroundColor: colors.blue_light,
+		borderRadius: 20,
+		position: 'relative',
+		bottom: 60
 	},
 	tipImage : {
-
+		height: 56,
+		width: 56,
 	},
 	tipText : {
-
+		flex: 1,
+		marginLeft: 20,
+		fontFamily: fonts.text,
+		color: colors.blue,
+		fontSize : 17,
+		textAlign : 'justify'
 	},
 	alertLabel : {
-
+		textAlign : 'center',
+		fontFamily: fonts.complement,
+		color: colors.heading,
+		fontSize: 12,
+		marginBottom : 5
 	}
 
 });
