697b06d8f3fdb238a47ab3cf9065ef5617755545
Vitor de Sousa <10253164+vitordesousa@users.noreply.github.com>
2021-04-26 02:39:23 -0300

    cadastrando a nova pagina no arquivo de rotas

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -8,6 +8,7 @@ import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
 import { PlantSelect } from '../pages/PlantSelect';
 import { PlantSave } from '../pages/PlantSave';
+import { MyPlants } from '../pages/MyPlants';
 
 /* 
 	Anotações:
@@ -30,6 +31,7 @@ const AppRoutes: React.FC = () => (
 		<stackRoutes.Screen name="Confirmation" component={Confirmation} />
 		<stackRoutes.Screen name="PlantSelect" component={PlantSelect} />
 		<stackRoutes.Screen name="PlantSave" component={PlantSave} />
+		<stackRoutes.Screen name="MyPlants" component={MyPlants} />
 	</stackRoutes.Navigator>
 )
 
